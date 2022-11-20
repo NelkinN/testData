dac21d393ec038fe6879c669062ac734035bdbe5
NelkinN <alexander.kuzzn@yandex.ru>
2022-11-20 20:25:11 +0300

    Create task1.js

---
+++ task1.js
@@ -0,0 +1,16 @@
+var barSeries = [{
+	name: 'Oil and gas extraction',
+	data: [13.93, 13.63, 13.73, 13.67]
+
+	}, {
+	name: 'Manufacturing industries and mining',
+	data: [12.24, 12.24, 11.95, 12.02]
+
+	}, {
+	name: 'Road traffic',
+	data: [10.00, 9.93, 9.97, 10.01]
+
+	}, {
+	name: 'Agriculture',
+	data: [4.35, 4.32, 4.34, 4.39]
+}]
